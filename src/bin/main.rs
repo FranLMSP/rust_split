16483d3cdf1d55d367b5c71401154e49f87c9221
Franco Colmenarez <info@francoacg.com>
2021-08-18 11:35:14 -0500

    Building a GTK app

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,17 +1,11 @@
-use std::time;
 use std::process;
 use rust_split::rs_timer::RsTimer;
 use rust_split::rs_split::RsSplit;
 
-use crossterm::{
-    event::{poll, read, Event, KeyCode},
-    terminal::{disable_raw_mode, enable_raw_mode},
-    Result,
-};
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow};
 
-
-
-fn main() -> Result<()> {
+fn main() {
     let splits = vec![
         RsSplit::new("First split", None, None),
         RsSplit::new("Second split", None, None),
@@ -22,48 +16,20 @@ fn main() -> Result<()> {
         process::exit(1);
     });
 
-    enable_raw_mode()?;
+    let app = Application::builder()
+        .application_id("org.gtk.rust_split")
+        .build();
+    
+    app.connect_activate(build_ui);
 
-    loop {
-        // Wait up to 1s for another event
-        if poll(time::Duration::from_millis(1_000))? {
-            // It's guaranteed that read() wont block if `poll` returns `Ok(true)`
-            let event = read()?;
-
-            // println!("Event::{:?}\r", event);
-
-            if event == Event::Key(KeyCode::Char(' ').into()) && !rs_timer.is_finished() {
-                match rs_timer.split() {
-                    Err(e) => eprintln!("Application error: {}", e),
-                    Ok(_) => (),
-                };
-            }
-
-            if event == Event::Key(KeyCode::Backspace.into()) {
-                match rs_timer.undo() {
-                    Err(e) => eprintln!("Application error: {}", e),
-                    Ok(_) => (),
-                };
-            }
-
-            if event == Event::Key(KeyCode::Tab.into()) {
-                match rs_timer.skip() {
-                    Err(e) => eprintln!("Application error: {}", e),
-                    Ok(_) => (),
-                };
-            }
-
-            if event == Event::Key(KeyCode::Esc.into()) {
-                break;
-            }
-        } else {
-            // Timeout expired, no event for 1s
-            // println!(".\r");
-        }
-    }
-
-    disable_raw_mode()?;
-
-    Ok(())
+    app.run();
 }
 
+fn build_ui(app: &Application) {
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("RustSplit")
+        .build();
+
+    window.present();
+}
